0fdafd9247be4089fa375a96750c3bed4bff096f
alexander.Khatamov <alexk2120@gmail.com>
2022-02-11 11:16:57 -0500

    background img w styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,28 @@ import React, { useState } from 'react'
 import Player from './Components/player'
 import Playlist from './Components/playlist'
 import playlist from './Components/playlist.data'
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from  "react-router-dom";
 
 
 function App() {
     const [activeSong, setActiveSong] = useState(playlist[0])
-  return (
-    <div className="app">
-        <Player activeSong={activeSong} />
-        <Playlist setActiveSong={setActiveSong} t/>
-    </div>
-  )
-}
 
+
+  
+  return (
+    <><div className="app">
+      <Player activeSong={activeSong} />
+      <Playlist setActiveSong={setActiveSong} t />
+    </div><div>
+        <BrowserRouter>
+          
+        </BrowserRouter>
+
+      </div></>
+  
+    
+    );
+  
+  }
 export default App
